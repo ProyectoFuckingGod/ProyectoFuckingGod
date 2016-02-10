2f6959ab41872e7c33e24e905d680114a41d5f03
1-DAM <1-DAM@Aula2-PC01>
2016-02-10 09:27:00 +0100

    Actualización en el Test. Valores y souts.

--- src/Test.java
+++ src/Test.java
@@ -7,7 +7,12 @@ public class Test {
     Persona persona2 = new Persona("Ozelito Tik Tak", "987654321");
     Persona persona3 = new Persona("Yeredaia", "147258369");
     Agenda agenda = new Agenda(4);
-
+    agenda.añadir(persona1);
+    agenda.añadir(persona2);
+    agenda.añadir(persona3);
+    String pers1="TKB2BLA";
+    String pers2="Ozelito Tik Tak";
+    String pers3="Yeredaia";
     System.out.printf("El teléfono de %s es: %s\n",pers1,agenda.getTelefono(pers1));
     System.out.printf("%s está en la posición %d del array\n",pers2, agenda.buscar(pers2));
     System.out.printf("En la posicion 1 del array se encuentra: %s\n", agenda.getPersona(1));
