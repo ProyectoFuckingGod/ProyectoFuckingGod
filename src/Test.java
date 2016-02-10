1f547d949759e14295fa4f5ce4d7b97ec41d3037
1-DAM <1-DAM@Aula2-PC01>
2016-02-10 09:25:05 +0100

    Actualización

--- src/Test.java
+++ src/Test.java
@@ -3,19 +3,22 @@ public class Test {
 
   public static void main(String[] args) {
 
-    Persona persona1 = new Persona("Juan", "956259749");
-    Persona persona2 = new Persona("Victor", "622131990");
-    Persona persona3 = new Persona("Noemi", "645987597");
+    Persona persona1 = new Persona("TKB2BLA", "123456789");
+    Persona persona2 = new Persona("Ozelito Tik Tak", "987654321");
+    Persona persona3 = new Persona("Yeredaia", "147258369");
     Agenda agenda = new Agenda(4);
     agenda.añadir(persona1);
     agenda.añadir(persona2);
     agenda.añadir(persona3);
-    System.out.printf("El teléfono de Victor es: %s\n", agenda.getTelefono("Victor"));
-    System.out.printf("Noemi está en la posición %d del array\n", agenda.buscar("Noemi"));
+    String pers1="TKB2BLA";
+    String pers2="Ozelito Tik Tak";
+    String pers3="Yeredaia";
+    System.out.printf("El teléfono de %s es: %s\n",pers1,agenda.getTelefono(pers1));
+    System.out.printf("%s está en la posición %d del array\n",pers2, agenda.buscar(pers2));
     System.out.printf("En la posicion 1 del array se encuentra: %s\n", agenda.getPersona(1));
     System.out.println(agenda.getPersona(2));
-    agenda.modificarTelefono("Victor", "659673974");
-    System.out.printf("El nuevo teléfono de Victor es %s\n", agenda.getTelefono("Victor"));
+    agenda.modificarTelefono(pers3, "659673974");
+    System.out.printf("El nuevo teléfono de %s es %s\n",pers3, agenda.getTelefono(pers3));
 
   }
 }
